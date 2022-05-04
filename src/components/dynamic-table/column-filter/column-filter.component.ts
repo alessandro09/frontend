a642235ce04c282eb4169626cec4615588fb4597
Alessandro Abegg <alessandro.abegg@outlook.com>
2022-05-04 12:28:23 -0300

    separação filtros dynamic-table

--- src/components/dynamic-table/column-filter/column-filter.component.ts
+++ src/components/dynamic-table/column-filter/column-filter.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input } from '@angular/core'
+
+import { DynamicTableColumnModel, DynamicTableModelFiltered, FilterModelCombobox } from '../dynamic-table.model'
 
 @Component({
   selector: 'app-column-filter',
   templateUrl: './column-filter.component.html',
   styleUrls: ['./column-filter.component.scss']
 })
-export class ColumnFilterComponent implements OnInit {
+export class ColumnFilterComponent {
+
+  @Input() column: DynamicTableColumnModel<any> = {} as any
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  filterOptions = () => (this.column as FilterModelCombobox).options
 
+  isFilterCombobox = () => (this.column as DynamicTableModelFiltered).filter === 'combobox'
+
+  filterType = (): any => (this.column as DynamicTableModelFiltered).filter
 }
